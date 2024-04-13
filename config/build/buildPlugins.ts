40a4a6fba4bf12da7715bc4a3dd7679a732fcffc
Жоголев Антон <anton.zhogolev@gazprombank.ru>
2024-04-13 12:43:42 +0300

    add bundleAnalizer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { type BuildOptions } from './types/config'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins (
     options: BuildOptions
@@ -18,6 +19,9 @@ export function buildPlugins (
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(options.isDev)
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
         })
     ]
     if (options.isDev) {
